7f16dbc8e06c249d8d27ef1d272d82555314d4f3
Piy020220 <149321564+Piy020220@users.noreply.github.com>
2023-11-05 21:13:56 +0100

    -вывод тексат масивов в html,добавление кнопки с вызовом функции и алерт

--- lesson-app/src/App.js
+++ lesson-app/src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+
 function App() {
   return (
+    
     <div className="App">
+       <div className='demo-container'>
+              <div>tekst massivov {aaa}</div><button onClick={onAddClick}>calculate</button>
+                </div> 
+           
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,9 +26,64 @@ function App() {
         >
           Learn React
         </a>
+        
       </header>
-    </div>
+      
+      </div>
+      
+    
   );
 }
 
 export default App;
+
+
+            let arr = ['ple','joeg','anton','vafledron']
+            console.log(arr[1])
+            
+            let index = 3;
+            console.log('dostal po indexu iz peremennoi', arr[index])
+            
+            for (let i = 0; i <arr.length; i++){
+
+                console.log('pod takim i',i,'vot takaja hujnia',arr[i]);
+            }
+             
+            console.log('=============================')
+
+            let kassa = [11,2,3,4,5]
+            let totalKassa = 0;
+            for (let i = 0; i <kassa.length; i++){
+                console.log(kassa[i])
+                 totalKassa = kassa[i] + totalKassa;
+                 console.log('total',totalKassa); 
+            }
+           
+            console.log('=============================')
+
+            let aaa = [[1,1],[2,2],[3,3,3,4],[4,4,2,3]]
+            
+            
+            const calculateKassa = (myKassa) => {
+               let totPr2 = 0;
+
+                for (let i = 0; i <myKassa.length; i++){
+                 totPr2 = myKassa[i] + totPr2;
+                 
+                }
+            return totPr2;
+            }
+            let kassaD = 0;
+            
+            const onAddClick = () => {
+            for (let i = 0; i < aaa.length; i++){
+
+                kassaD = calculateKassa(aaa[i]) + kassaD;
+                console.log(kassaD);
+              alert(kassaD)  
+              }          
+
+              }
+
+            
+           
