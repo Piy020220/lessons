50a861bdec1f1c093012569262ed67b43b6fd4f6
Piy020220 <149321564+Piy020220@users.noreply.github.com>
2023-12-19 20:05:58 +0100

    -render

--- lesson-app/src/Store/StoreProductList/StoreProductList.jsx
+++ lesson-app/src/Store/StoreProductList/StoreProductList.jsx
@@ -1,14 +1,40 @@
+import {Header} from '../../components/Header'
+import React, { useState } from 'react';
 import { StoreProductItem } from "../StoreProductItem/StoreProductItem"
 import './StoreProductList.scss'
 
 
-export const StoreProductList = ({products}) => {
-    console.log('StoreProductList Products :', products)
+export const StoreProductList = (props) => {
+
+   const {products, userCountry} = props;
     
+    console.log('StoreProductList props.Products:', props.products)
+    console.log('StoreProductList Products:', products)
+    
+    const [user, setUser] = useState({ 
+      name:'Vlad', 
+      surname: 'Legun',
+      country:'pl'
+    })
     return(
+        <><Header/><div> 
+          <span className='product-title vladik'>{user.name}</span>, 
+          <span className='vladik'>{user.surname}</span>
+          
+          
+          <select defaultValue={user.country} name="Country" id="country" onChange={event => setUser({...user,country: event.target.value})}>
+            <option value="belka">belka</option>
+            <option value="ua">Ua</option>
+            <option value="pl">Pl</option>
+          </select>
+          <div>Selected country: {user.country}</div>
+          
+           
+           
+     </div> 
         <ul className="store-product-list">
-            {products.map((product) => <StoreProductItem product={product} key={product.id}/>)}
-       </ul>
+          {products.map((product) => <StoreProductItem product={product} userCountry={userCountry} key={product.id}/>)}
+       </ul></>
     )
 }
       
